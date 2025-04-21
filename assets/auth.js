df8c60c80e892fc60c16d4dd01b1ee495446e228
ali <nabiy5511@gmail.com>
2025-04-21 14:09:49 +0500

    fixed :D

--- assets/auth.js
+++ assets/auth.js
@@ -26,13 +26,13 @@ form.addEventListener('submit', async (event) => {
             const data = await response.json()
             const role = data.data.user.role
             if (role == 'Admin') {
-                return window.location.href = `https://cp.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cp.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Cook') {
-                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cook.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Chef') {
-                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cook.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Waiter' || role == 'Waitress') {
-                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://waiter.aif.uz/identificate/index.html?token=${data.token}`
             }
         } else {
             alert('Login or password is wrong!');
