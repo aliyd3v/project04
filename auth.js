8cd00397ca4ed6f940ac614f2465b6e0f84f86b4
ali <nabiy5511@gmail.com>
2025-04-21 13:50:35 +0500

    First commit

---
+++ auth.js
@@ -0,0 +1,85 @@
+const form = document.getElementById('login_form');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const apiUrl = 'https://api.aif.uz/login';
+
+    const requestData = {
+        username: username,
+        password: password
+    };
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        if (response.ok) {
+            const data = await response.json()
+            const role = data.data.user.role
+            if (role == 'Admin') {
+                return window.location.href = `https://cp.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Cook') {
+                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Chef') {
+                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Waiter' || role == 'Waitress') {
+                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+            }
+        } else {
+            alert('Login or password is wrong!');
+        }
+    } catch (error) {
+        alert('Internal server error!');
+    }
+});
+
+async function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const apiUrl = 'https://api.aif.uz/login';
+
+    const requestData = {
+        username: username,
+        password: password
+    };
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        if (response.ok) {
+            const data = await response.json()
+            const role = data.user.role
+            if (role == 'Admin') {
+                return window.location.href = `https://cp.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Cook') {
+                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Chef') {
+                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Waiter') {
+                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+            } else if (role == 'Waitress') {
+                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+            }
+        } else {
+            alert('Login or password is wrong!');
+        }
+    } catch (error) {
+        alert('Internal server error!');
+    }
+}
