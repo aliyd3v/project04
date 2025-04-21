709830e8853462030160386bf0d845ca63514884
ali <nabiy5511@gmail.com>
2025-04-21 14:11:49 +0500

    fixed

--- assets/auth.js
+++ assets/auth.js
@@ -66,15 +66,15 @@ async function login() {
             const data = await response.json()
             const role = data.user.role
             if (role == 'Admin') {
-                return window.location.href = `https://cp.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cp.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Cook') {
-                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cook.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Chef') {
-                return window.location.href = `https://cook.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://cook.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Waiter') {
-                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://waiter.aif.uz/identificate/index.html?token=${data.token}`
             } else if (role == 'Waitress') {
-                return window.location.href = `https://waiter.api.uz/identificate/index.html?token=${data.token}`
+                return window.location.href = `https://waiter.aif.uz/identificate/index.html?token=${data.token}`
             }
         } else {
             alert('Login or password is wrong!');
